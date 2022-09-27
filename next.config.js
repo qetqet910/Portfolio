b2aa90b6dd9f46b0fc7bb7701d5e6edc1d2fc59a
qetqet910 <79036088+qetqet910@users.noreply.github.com>
2022-09-27 17:46:29 +0900

    Link Route Modify

--- next.config.js
+++ next.config.js
@@ -5,7 +5,7 @@ const repository = 'https://introduceme.kr';
 
 const nextConfig = {
   reactStrictMode: true,
-  assetPrefix: !debug ? `/${repository}/` : '',
+  assetPrefix: !debug ? `${repository}/` : '',
   trailingSlash: true,
   images: {
     loader: 'akamai',
