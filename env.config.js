030d1eaf9f5d5b22d671feac9ffee93d4a018c33
qetqet910 <79036088+qetqet910@users.noreply.github.com>
2022-09-27 16:32:54 +0900

    Del

--- env.config.js
+++
@@ -1,6 +0,0 @@
-const debug = process.env.NODE_ENV !== 'production';
-const repository = 'https://introduceme.kr';
-
-module.exports = {
-  'process.env.BACKEND_URL': !debug ? `/${repository}` : '',
-};
