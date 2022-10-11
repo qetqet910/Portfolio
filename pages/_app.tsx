9e6835165111a27e7663026cf936e9223eb5a37c
qetqet910 <79036088+qetqet910@users.noreply.github.com>
2022-10-11 13:59:01 +0900

    og:image

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -9,7 +9,7 @@ function MyApp({ Component, pageProps }: AppProps) {
         <meta property="og:type" content="website" />
         <meta property="og:url" content="https://introduceme.kr" />
         <meta property="og:title" content="Portfolio" />
-        <meta property="og:image" content="../public/img/webdev.png" />
+        <meta property="og:image" content="https://raw.githubusercontent.com/qetqet910s/Portfolio/main/public/img/webdev.png?token=GHSAT0AAAAAABYHMZGSL7HXTYWDZIQLKSBYY2FAX3A" />
         <meta property="og:description" content="포트폴리오 웹 페이지" />
         <meta property="og:site_name" content="나를 소개합니다" />
         <meta property="og:locale" content="en_US" />
