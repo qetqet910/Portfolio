c6ca455c2ce67fe6e35f81429c5d74d2ab9485b1
qetqet910 <79036088+qetqet910@users.noreply.github.com>
2022-10-11 13:55:23 +0900

    gh-pages

--- _next/static/5u0YUg6g3uaLeCbpS_Zmp/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/ae0e3e027412e072.css","static/chunks/pages/index-989534e9848db494.js"],"/_error":["static/chunks/pages/_error-a4ba2246ff8fb532.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
