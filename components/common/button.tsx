0eb0fd00d7f1e9e9069514c91383a9c31e148d2c
qetqet910 <79036088+qetqet910@users.noreply.github.com>
2022-10-11 16:54:39 +0900

    Component

---
+++ components/common/button.tsx
@@ -0,0 +1,9 @@
+import styles from '../styles/Home.module.scss'
+
+const LargeButton = () => {
+    return (
+        <div className={styles.minuButton}></div>
+    )
+}
+
+export default LargeButton
